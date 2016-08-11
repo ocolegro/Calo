9a9162be7db0c06a170b74ce2994427d1b4dcb7e
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-08-11 15:36:19 -0700

    check

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -81,6 +81,9 @@ void SteppingAction::UserSteppingAction(const G4Step* aStep) {
 	if(trackID == 1){
 		G4cout << "The preMom is " << preMom[0] << ", " << preMom[1] << ", " << preMom[2] << G4endl;
 		G4cout << "The passPrePos is " << passPrePos[0] << ", " << passPrePos[1] << ", " << passPrePos[2] << G4endl;
+		G4cout << "The passPrePos is " << passPrePos[0]*lenUnit << ", " << passPrePos[1]*lenUnit << ", " << passPrePos[2]*lenUnit << G4endl;
+		G4cout << "The passPrePos is " << passPrePos[0]/lenUnit << ", " << passPrePos[1]/lenUnit << ", " << passPrePos[2]/lenUnit << G4endl;
+
 		G4cout << "The bFieldPre is " << bFieldPre[0]/gauss  << ", " << bFieldPre[1] /gauss << ", " << bFieldPre[2] /gauss << G4endl;
 		G4cout << "The bFieldPre is " << bFieldPre[0]  << ", " << bFieldPre[1]  << ", " << bFieldPre[2]  << G4endl;
 
