5696d4aed30b88b1772add710eac6dac418d46df
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-08-11 16:33:45 -0700

    check

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -90,12 +90,12 @@ void SteppingAction::UserSteppingAction(const G4Step* aStep) {
 		G4cout << "The postMom is " << postMom[0] << ", " << postMom[1] << ", " << postMom[2] << G4endl;
 
 	}*/
-	for (int i = 0; i <90; i ++)
+	for (int i = 0; i <150; i ++)
 	{
 		double bFieldPre[3] = {999,0,0};
 		passPrePos[0] = 0;
 		passPrePos[1] = 0;
-		passPrePos[2] = -15 * 10* centimeter + i*10*centimeter;
+		passPrePos[2] = i*.1*centimeter;
 		G4cout << "making a field call w/ z = " << passPrePos[2]/centimeter << G4endl;
 		G4TransportationManager::GetTransportationManager()->GetFieldManager()->GetDetectorField()->GetFieldValue(passPrePos, bFieldPre);
 
