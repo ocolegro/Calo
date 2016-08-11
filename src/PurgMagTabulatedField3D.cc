29bf8199e35ede959b5e52330ebaf6ed0319a7c9
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-08-11 16:38:49 -0700

    check

--- src/PurgMagTabulatedField3D.cc
+++ src/PurgMagTabulatedField3D.cc
@@ -205,7 +205,7 @@ void PurgMagTabulatedField3D::GetFieldValue(const G4double point[4],
   G4double x = point[0]/lenUnit;
   G4double y = point[1]/lenUnit;
   G4double ztrue = point[2]/lenUnit ;//+ fZoffset)/lenUnit ;
-  G4double z = 15 * lenUnit - ztrue;
+  G4double z = 150 * lenUnit - ztrue;
   G4cout << "The maxz is " << maxz << G4endl;
   bool printField = false;
   if (Bfield[0] == 999){
