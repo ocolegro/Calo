70d0610950137585953b39604b1f8bcc9f52cd11
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-08-11 14:44:26 -0700

    check

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -42,7 +42,7 @@ DetectorConstruction::DetectorConstruction(G4int ver, G4int mod) :
 	DefineMaterials();
 	m_detectorMessenger = new DetectorMessenger(this);
 	UpdateCalorSize();
-    SetMagField("b18d36.dat",-m_CalorSizeZ*mm);
+    SetMagField("b18d36.dat",-0.5 * (Detector->GetCalorSizeZ()));
 
 }
 void DetectorConstruction::buildTracker(){
