3ce92b9a74cd2706c27efe5bdd462c952f94aa41
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-08-11 17:32:46 -0700

    check

--- src/PurgMagTabulatedField3D.cc
+++ src/PurgMagTabulatedField3D.cc
@@ -135,7 +135,7 @@ PurgMagTabulatedField3D::PurgMagTabulatedField3D(const char* filename,
         for(unsigned ix=0; ix < (nxy-2); ix++){
         	//G4cout << "ix = " << ix << ", nxy = " << nxy << G4endl;
             bval = stod(tokens.at(ix+1)) * gauss;
-            G4cout << "The field in the y direction at iz = " << iz << ", ix = " << ix << " = " << bval << G4endl;
+            G4cout << "The field in the y direction at iz = " << iz << ", ix = " << ix << " = " << bval /gauss << G4endl;
 
             yField[ix][0][iz] = bval * fieldUnit;
             xField[ix][0][iz] = 0.0  * fieldUnit;
@@ -250,7 +250,7 @@ void PurgMagTabulatedField3D::GetFieldValue(const G4double point[4],
       zField[xindex+1][yindex+1][zindex+1] *    xlocal  *    ylocal  *    zlocal ;
     if (printField){
     	G4cout << "The x,y,z that we are reading in is: " << x/cm << ", " << y/cm << ", " << ztrue/cm << G4endl;
-    	G4cout << "The recalled filed, before passing was :  "  << " (" << Bfield[0]*gauss << ", " << Bfield[1]*gauss << ", " << Bfield[2]*gauss << " )" << G4endl;
+    	G4cout << "The recalled filed, before passing was :  "  << " (" << Bfield[0]/gauss << ", " << Bfield[1]/gauss << ", " << Bfield[2]/gauss << " )" << G4endl;
     }
   } else {
     Bfield[0] = 0.0;
