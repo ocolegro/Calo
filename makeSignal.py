4c9818d1c8712ebf278db102fbdc604398a0be6b
owen colegrove <ocolegro@cmslpc31.fnal.gov>
2016-08-17 19:44:26 -0500

    up

--- makeSignal.py
+++ makeSignal.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import os
-absPath = '/uscms/home/ocolegro/Calo/signal/4GeV/'
-outDir  = '/eos/uscms/store/user/ocolegro/signalInc_test'
-files = os.listdir('signal/4GeV')
+absPath = '/uscms/home/ocolegro/Calo/signal/4GeV_nodecay_modified/'
+outDir  = '/store/user/ocolegro/4GeV_nodecay_magOn_run2/'
+files = os.listdir('signal/4GeV_nodecay_modified')
 #python submitProdLPC_signal.py -f /uscms/home/ocolegro/Calo/signal/4GeV/SLAC.4GeV.W.mchi.0.05.map.0.15.alpha0.1.fermionDM_unweighted_events_modified.lhe -s 1 -e /eos/uscms/store/user/ocolegro/signal
-for file in files[0:2]:
-    os.system('python submitProdLPC_signal.py -f %s/%s -s 0 -e %s' % (absPath,file,outDir))
+for file in files:
+    	print 'Calling %s ' % ('python submitProdLPC_signal.py -f %s/%s -s 0 -e %s' % (absPath,file,outDir))
+	os.system('python submitProdLPC_signal.py -f %s/%s -s 0 -e %s -n 100000' % (absPath,file,outDir))
